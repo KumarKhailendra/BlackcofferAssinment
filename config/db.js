c9eb54bdedad137d912de2cc7a717119612c322f
KumarKhailendra <khailendrakumar417@gmail.com>
2023-10-18 12:59:45 +0530

    complete data Analytics part

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ function connectDB(){
     socketTimeoutMS: 60000,
   };
   
-  // const mongoURL = process.env.MONGODB_URL;
+  const mongoURL = process.env.MONGODB_URL;
   // const mongoURL = "mongodb://0.0.0.0:27017/coffee";
   
   mongoose.connect(mongoURL, options) 

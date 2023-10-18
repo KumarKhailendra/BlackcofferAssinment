44b685f4a0b0a589b69b6eef35c784397f60b921
KumarKhailendra <khailendrakumar417@gmail.com>
2023-10-18 12:55:52 +0530

    complete data Analytics part

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const AllData = require("./route/AllData");
 
 const app = express();
 dotenv.config();
 connectDB();
-
+const corsOptions = {
+    origin: process.env.URL
+}
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send("API is running..");
